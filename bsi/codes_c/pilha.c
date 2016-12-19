6a119a710612d666a0577b3ccad13d6128a16a76
ymlourenco <ymlourenco@gmail.com>
2016-12-18 22:29:28 -0200

    [UPDATED] Inserido casos de teste para testar se o pilha.h está funcionando corretamente

--- bsi/codes_c/pilha.c
+++ bsi/codes_c/pilha.c
@@ -10,7 +10,15 @@ PILHA *pilha = (PILHA*)malloc(sizeof(PILHA));
     create(pilha);
     IsEmpty(pilha);
     SizePilha(pilha);
-
+    Insere(pilha, 5);
+    SizePilha(pilha);
+    Insere(pilha, 3);
+    SizePilha(pilha);
+    Insere(pilha, 2);
+    SizePilha(pilha);
+    imprime(pilha);
+    Insere(pilha, 7);
+    imprime(pilha);
 
 	return 0;
 }
